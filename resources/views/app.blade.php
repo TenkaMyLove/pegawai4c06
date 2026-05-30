6d8d86cf672338a443174cc86233f28624c80df9
Muhammad Darrell Febrian Yuza <stevenfebriann@gmail.com>
2026-05-30 19:51:09 +0800

    Update title and script reference in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pegawai App</title>
+    <title>SIMPADU Pegawai</title>
 
-    @vite(['resources/js/style.css'])
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div id="app"></div>
